6bde3b6da5ada28f10a742566451e91c64786dc0
nutty001 <90544250+nutty001@users.noreply.github.com>
2022-11-14 12:58:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;//Random number between 1-6
+var randomDiceimage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+var randomImagesource = "images/" + randomDiceimage; //images/dice1.png-images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImagesource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+
+  document.querySelector("h1").innerHTML = "🚩Player1 Wins!!!";
+}
+
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player2 Wins!!!🚩";
+}
+
+else{
+  document.querySelector("h1").innerHTML= "❌Draw❌";
+}
